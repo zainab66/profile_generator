922f9cf85ddcb5ede3a8d54af3b8317398647bd1
zainab66 <zainab66r@yahoo.com>
2020-08-25 04:00:47 +0000

    Fun Profile Generator

---
+++ survey.js
@@ -0,0 +1,88 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+ 
+
+
+const r2 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r2.question('What is your name? ', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+
+
+
+const r3 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r3.question('What is an activity you like doing? ', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+
+
+
+
+const r4 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r4.question('What do you listen to while doing that? ', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+
+
+const r5 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r5.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+
+
+
+const r6 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r6.question('What is your favourite thing to eat for that meal? ', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+
+
+const r7 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r7.question('hich sport is your absolute favourite?', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+
+
+const r8 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+r8.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+
+
+    r8.close();
+  });});});});});});});});
